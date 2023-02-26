09179ecebb498b01e8f7cd8c684d5a28ab8ba4a1
shinjytroy <nguyenquangsang1991@gmail.com>
2023-02-26 10:16:49 +0700

    view product

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -46,8 +46,9 @@
                   <th style="width: 10%">Category</th>
                   <th style="width: 10%">Brand</th>
                   <th style="width: 20%">Name</th>
-                  <th style="width: 5%">Regular Price</th>  
-                  <th style="width: 10%">Status</th>
+                  <th style="width: 5%">Regular Price</th>
+                  <th style="width: 5%">Sale Price</th>  
+                  <th style="width: 5%">Status</th>
                   <th style="width: 5%">Featured</th>
                   <th style="width: 5%">Stock</th>
                   <th style="width: 5%">Image</th>
